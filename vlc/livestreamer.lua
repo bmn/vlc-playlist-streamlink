3663ab8a229cefd18404cdaa2d2d67552bfdc8f4
U-IAN-LAP\xbox <webdev@ianbennett.net>
2015-11-20 13:35:03 +0000

    Add livestreamer.lua

---
+++ vlc/livestreamer.lua
@@ -0,0 +1,18 @@
+--[[
+	Livestreamer playlist plugin by bmn.
+	Requires a running livestreamersrv instance on port 88 (default).
+		<https://github.com/athoik/livestreamersrv> by athoik
+--]]
+
+function probe()
+    if vlc.access ~= "http" and vlc.access ~= "https" then
+        return false
+    end
+    activate = string.match( string.sub( vlc.path, 1, 7 ), "twitch" ) or string.find( vlc.path, ".twitch.tv" )
+			or string.match( string.sub( vlc.path, 1, 7 ), "hitbox" ) or string.find( vlc.path, ".hitbox.tv" )
+	return activate
+end
+
+function parse()
+	return {{ path = 'http://127.0.0.1:88/' .. vlc.path, title = vlc.access .. '://' .. vlc.path }}
+end
